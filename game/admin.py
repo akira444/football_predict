386779da8cecb609042a808d083f0521d2b7575d
Akira Yokoyama <akirayokoyama999@gmail.com>
2021-02-14 16:58:27 +0100

    Final CSIA - added Game admin

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PlayerStatus, AvailableLeague
+from .models import PlayerStatus, AvailableLeague, Game
 
 # Register models
 admin.site.register(PlayerStatus)
 admin.site.register(AvailableLeague)
+admin.site.register(Game)
